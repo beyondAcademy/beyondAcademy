1557ea749a162305520dbc2f8d559785e75c28c7
beyondAcademy <beyondAcademy20191201@gmail.com>
2019-11-26 22:39:21 +0900

    tmp commit

--- beyondAcademy/settings.py
+++ beyondAcademy/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 if DEBUG:
     STATIC_URL  = '/static/'
-    #STATIC_ROOT = 'static'
+    STATIC_ROOT = 'static'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
