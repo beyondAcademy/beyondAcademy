494490569f7f8b81354900bfcbbe736075ef5f7a
beyondAcademy <beyondAcademy20191201@gmail.com>
2019-12-09 00:36:03 +0900

    tmp commit

--- manage.py
+++ manage.py
@@ -6,14 +6,15 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beyondAcademy.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
+    # try:
+    #     from django.core.management import execute_from_command_line
+    # except ImportError as exc:
+    #     raise ImportError(
+    #         "Couldn't import Django. Are you sure it's installed and "
+    #         "available on your PYTHONPATH environment variable? Did you "
+    #         "forget to activate a virtual environment?"
+    #     ) from exc
     execute_from_command_line(sys.argv)
 
 
